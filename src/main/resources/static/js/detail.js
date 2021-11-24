3dd0373bd829dc21c6e115f768bdd08aaba4354c
kqingcan <16302010033@fudan.edu.cn>
2021-11-25 02:45:43 +0800

    finish 2 pages

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -3,7 +3,8 @@ $(document).ready(function () {
     var path_spilt = path.split("/")
     var id = path_spilt[path_spilt.length-1]
     var detail = $("#detail_info")
-
+    var detail_table = $("#detail_table")
+    var detail_title = $("#detail_title")
     function getDetail(){
         $.ajax({
             // url : '/initTable',
@@ -25,20 +26,51 @@ $(document).ready(function () {
                     var abstracts = item["abstracts"];
                     var year = item["year"]
                     var link = item["link"]
-                    var option = "<dl class=\"list-group\">\n" +
-                        "                    <dt>\n" +
-                        "                        <a class=\"standard-title\" href=\""+"/detail/"+detail_id+"\">\n" +
-                        "                        <h4 class=\"list-group-item-heading\" >"+ standard +"</h4>\n" +
-                        "                        </a>\n" +
-                        "                    </dt>\n" +
-                        "                    <span class=\"label label-default\">行业: "+ industry +"</span>\n" +
-                        "                    <span class=\"label label-info\">国家:"+ region +"</span>\n" +
-                        "                    <span class=\"label label-info\">标准号: "+ number +"</span>\n" +
-                        "                    <span class=\"label label-info\">是否现行: "+effectiveness+"</span>\n" +
-                        "                    <span class=\"label label-info\">范围: "+scope+"</span>\n" +
-                        "         </dl>"+
-                        "         <hr class=\"simple\" color=\"#6f5499\" />"
-                    detail.append(option)
+                    if (number == null){
+                        number = "无"
+                    }
+                    if(abstracts ==null){
+                        abstracts = "暂无简介"
+                    }
+                    if(standard ==null){
+                        standard = "暂未命名"
+                    }
+                    var title_option = "<h3>"+standard+"</h3>"
+                    detail_title.append(title_option)
+                    var option = " <tbody>\n" +
+                        "\n" +
+                        "                <tr>\n" +
+                        "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">行业：</td>\n" +
+                        "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+ industry+"</td>\n" +
+                        "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">国家：</td>\n" +
+                        "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\" >"+region+"</td>\n" +
+                        "                </tr>\n" +
+                        "                <tr>\n" +
+                        "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">标准号：</td>\n" +
+                        "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+number+"</td>\n" +
+                        "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">年份：</td>\n" +
+                        "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+year+"</td>\n" +
+                        "                </tr>\n" +
+                        "                <tr>\n" +
+                        "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">是否现行：</td>\n" +
+                        "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+effectiveness+"</td>\n" +
+                        "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">范围：</td>\n" +
+                        "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+scope+"</td>\n" +
+                        "                </tr>\n" +
+                        "                <tr>\n" +
+                        "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">标准名称：</td>\n" +
+                        "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+standard+"</td>\n" +
+                        "                </tr>\n" +
+                        "                <tr>\n" +
+                        "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">链接：</td>\n" +
+                        "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\"><a href='"+link+"'>"+link+"</a></td>\n" +
+                        "                </tr>\n" +
+                        "                <tr>\n" +
+                        "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">主要内容：</td>\n" +
+                        "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+abstracts+"</td>\n" +
+                        "                </tr>\n" +
+                        "                </tbody>"
+                    detail_table.append(option)
                     // var option = "<option>"+item+"</option>"
                     // console.log(option)
 
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -24,17 +24,23 @@ $(document).ready(function () {
                     var effectiveness = item["effectiveness"]
                     var scope = item["scope"];
                     var standard = item["standard"];
-                    var option = "<dl class=\"list-group\">\n" +
-                        "                    <dt>\n" +
-                        "                        <a class=\"standard-title\" href=\""+"/detail/"+detail_id+"\">\n" +
+                    if (number == null){
+                        number = "无"
+                    }
+                    if(standard ==null){
+                        standard = "暂未命名"
+                    }
+                    var option = "<dl class=\"list-group\"  style='margin-bottom: 3%'>\n" +
+                        "                    <dt style='margin-bottom: 2%' >\n" +
+                        "                        <a class=\"standard-title\" href=\""+"/detail/"+detail_id+"\" target='_blank'>\n" +
                         "                        <h4 class=\"list-group-item-heading\" >"+ standard +"</h4>\n" +
                         "                        </a>\n" +
                         "                    </dt>\n" +
-                        "                    <span class=\"label label-default\">行业: "+ industry +"</span>\n" +
-                        "                    <span class=\"label label-info\">国家:"+ region +"</span>\n" +
-                        "                    <span class=\"label label-info\">标准号: "+ number +"</span>\n" +
-                        "                    <span class=\"label label-info\">是否现行: "+effectiveness+"</span>\n" +
-                        "                    <span class=\"label label-info\">范围: "+scope+"</span>\n" +
+                        "                    <span class=\"label label-default\">行业:    "+ industry +"</span>\n" +
+                        "                    <span class=\"label label-info\">国家:    "+ region +"</span>\n" +
+                        "                    <span class=\"label label-info\">标准号:    "+ number +"</span>\n" +
+                        "                    <span class=\"label label-info\">是否现行:    "+effectiveness+"</span>\n" +
+                        "                    <span class=\"label label-info\">范围:    "+scope+"</span>\n" +
                         "         </dl>"+
                         "         <hr class=\"simple\" color=\"#6f5499\" />"
                     // var option = "<option>"+item+"</option>"
