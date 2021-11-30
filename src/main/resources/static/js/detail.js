7875e751e34def929fac663584f8cd1fa8c8e8e4
kqingcan <16302010033@fudan.edu.cn>
2021-11-30 23:16:17 +0800

    change language

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -5,16 +5,77 @@ $(document).ready(function () {
     var detail = $("#detail_info")
     var detail_table = $("#detail_table")
     var detail_title = $("#detail_title")
-    function getDetail(){
+
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+        }
+        return "";
+    }
+
+    $(function () {
+
+        $("#chinese").click(function () {
+            setCookie("language", "chinese", 7)
+            window.location.reload();
+        });
+        $("#english").click(function () {
+            setCookie("language", "english", 7)
+            window.location.reload();
+        });
+
+        var lang = getCookie("language")
+
+        if (lang != "chinese" && lang !="english"){
+            setCookie("language", "english", 7);
+        }
+
+        if (lang == "chinese") {
+            $("#page-title").html("“一带一路”工业清洁生产指标体系与标准数据平台");
+            $("#population").html("人口");
+            $("#standard").html("标准");
+            $("#others").html("其他");
+            $("#search-filed").placeholder = "搜索本页";
+            $("#search").html("搜索");
+            $("#login").html("登录");
+            $("#register").html("注册");
+            $("#panel-title").html("造纸行业标准");
+            $("#language").html("语言<b class=\"caret\"></b>");
+            $("#chinese").html("中文");
+            $("#english").html("英文")
+            $("#detail-words").html("详细信息")
+
+
+        }
+        getDetail(lang)
+    })
+
+    function getDetail(language){
+        var url= "/getDetail/"
+        if(language=="english"){
+            url="/getDetail/"
+        }
         $.ajax({
             // url : '/initTable',
-            url: '/getDetail/'+id,
+            url: url+id,
             type : 'get',
             dataType : "json",
             async : true,
             success : function(returnValue) {
                 var arr = returnValue ;
                 console.log(arr)
+                detail_title.html("")
                 $.each(arr, function(i, item) {
                     var detail_id = item["detail_id"];
                     var industry = item["industry"];
@@ -26,6 +87,7 @@ $(document).ready(function () {
                     var abstracts = item["abstracts"];
                     var year = item["year"]
                     var link = item["link"]
+
                     if (number == null){
                         number = "无"
                     }
@@ -35,6 +97,18 @@ $(document).ready(function () {
                     if(standard ==null){
                         standard = "暂未命名"
                     }
+                    if(language=="english"){
+                        if (number == null){
+                            number = "null"
+                        }
+                        if(abstracts ==null){
+                            abstracts = "null"
+                        }
+                        if(standard ==null){
+                            standard = "null"
+                        }
+                    }
+
                     var title_option = "<h3>"+standard+"</h3>"
                     detail_title.append(title_option)
                     var option = " <tbody>\n" +
@@ -69,7 +143,42 @@ $(document).ready(function () {
                         "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">主要内容：</td>\n" +
                         "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+abstracts+"</td>\n" +
                         "                </tr>\n" +
-                        "                </tbody>"
+                        "                </tbody>";
+                    if(language=="english"){
+                        option = " <tbody>\n" +
+                            "\n" +
+                            "                <tr>\n" +
+                            "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">Industry：</td>\n" +
+                            "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+ industry+"</td>\n" +
+                            "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">Region：</td>\n" +
+                            "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\" >"+region+"</td>\n" +
+                            "                </tr>\n" +
+                            "                <tr>\n" +
+                            "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">Number：</td>\n" +
+                            "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+number+"</td>\n" +
+                            "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">Year：</td>\n" +
+                            "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+year+"</td>\n" +
+                            "                </tr>\n" +
+                            "                <tr>\n" +
+                            "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">Effectiveness：</td>\n" +
+                            "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+effectiveness+"</td>\n" +
+                            "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">Scope：</td>\n" +
+                            "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+scope+"</td>\n" +
+                            "                </tr>\n" +
+                            "                <tr>\n" +
+                            "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">Standard：</td>\n" +
+                            "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+standard+"</td>\n" +
+                            "                </tr>\n" +
+                            "                <tr>\n" +
+                            "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">Link：</td>\n" +
+                            "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\"><a href='"+link+"'>"+link+"</a></td>\n" +
+                            "                </tr>\n" +
+                            "                <tr>\n" +
+                            "                    <td style=\"width: 18% ; padding-bottom: 30px;font-weight: bold\">Abstracts：</td>\n" +
+                            "                    <td style=\"width: 32% ; padding-bottom: 30px; text-align: left\">"+abstracts+"</td>\n" +
+                            "                </tr>\n" +
+                            "                </tbody>";
+                    }
                     detail_table.append(option)
                     // var option = "<option>"+item+"</option>"
                     // console.log(option)
@@ -79,5 +188,5 @@ $(document).ready(function () {
             }
         });
     }
-    getDetail()
+
 })
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,21 +1,79 @@
 $(document).ready(function () {
 
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+        }
+        return "";
+    }
+
     var years = new Array()
     $(function () {
+        // setCookie("language", "english", 7)
+        $("#chinese").click(function () {
+            setCookie("language", "chinese", 7)
+            window.location.reload();
+        });
+        $("#english").click(function () {
+            setCookie("language", "english", 7)
+            window.location.reload();
+        });
+
+        var lang = getCookie("language")
+        // $("#title-en").html(function (){
+        //     return "“一带一路”工业清洁生产指标体系与标准数据平台"
+        // })
+        if (lang != "chinese" && lang !="english"){
+            setCookie("language", "english", 7);
+        }
+        if (lang == "chinese") {
+            $("#page-title").html("“一带一路”工业清洁生产指标体系与标准数据平台");
+            $("#population").html("人口");
+            $("#standard").html("标准");
+            $("#others").html("其他");
+            $("#search-filed").placeholder = "搜索本页";
+            $("#search").html("搜索");
+            $("#login").html("登录");
+            $("#register").html("注册");
+            $("#panel-title").html("造纸行业标准");
+            $("#language").html("语言<b class=\"caret\"></b>");
+            $("#chinese").html("中文");
+            $("#english").html("英文")
+
+        }
+        getList(lang)
     })
 
 
-
-    function getList(){
+    function getList(language){
+        console.log(language)
+        var url="/getList";
+        if(language=="english"){
+            url="/getList";
+        }
+        // console.log(language)
         $.ajax({
             // url : '/initTable',
-            url: '/getList',
+            url: url,
             type : 'get',
             dataType : "json",
             async : true,
             success : function(returnValue) {
                 var arr = returnValue ;
-                console.log(arr)
+                // console.log(arr)
+                // console.log(language)
+                $("#standard-list").html("");
                 $.each(arr, function(i, item) {
                     var detail_id = item["detail_id"];
                     var industry = item["industry"];
@@ -26,10 +84,18 @@ $(document).ready(function () {
                     var standard = item["standard"];
                     if (number == null){
                         number = "无"
+                        if (language=="english"){
+                            number = "null"
+                        }
                     }
                     if(standard ==null){
                         standard = "暂未命名"
+                        if (language=="english"){
+                            // console.log("in if:"+language)
+                            standard="null"
+                        }
                     }
+
                     var option = "<dl class=\"list-group\"  style='margin-bottom: 3%'>\n" +
                         "                    <dt style='margin-bottom: 2%' >\n" +
                         "                        <a class=\"standard-title\" href=\""+"/detail/"+detail_id+"\" target='_blank'>\n" +
@@ -43,6 +109,21 @@ $(document).ready(function () {
                         "                    <span class=\"label label-info\">范围:    "+scope+"</span>\n" +
                         "         </dl>"+
                         "         <hr class=\"simple\" color=\"#6f5499\" />"
+                    if(language=="english"){
+                        option = "<dl class=\"list-group\"  style='margin-bottom: 3%'>\n" +
+                            "                    <dt style='margin-bottom: 2%' >\n" +
+                            "                        <a class=\"standard-title\" href=\""+"/detail/"+detail_id+"\" target='_blank'>\n" +
+                            "                        <h4 class=\"list-group-item-heading\" >"+ standard +"</h4>\n" +
+                            "                        </a>\n" +
+                            "                    </dt>\n" +
+                            "                    <span class=\"label label-default\">Industry:    "+ industry +"</span>\n" +
+                            "                    <span class=\"label label-info\">Region:    "+ region +"</span>\n" +
+                            "                    <span class=\"label label-info\">Number:    "+ number +"</span>\n" +
+                            "                    <span class=\"label label-info\">Effectiveness:    "+effectiveness+"</span>\n" +
+                            "                    <span class=\"label label-info\">Scope:    "+scope+"</span>\n" +
+                            "         </dl>"+
+                            "         <hr class=\"simple\" color=\"#6f5499\" />"
+                    }
                     // var option = "<option>"+item+"</option>"
                     // console.log(option)
 
@@ -51,7 +132,8 @@ $(document).ready(function () {
             }
         });
     }
-    getList()
+
+
 
 
 
